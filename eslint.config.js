55ab2bfd241fd4c6946a5a07b6531d284196bd90
Md Kamre Aallam <mdkamreaallam786@gmail.com>
2026-04-06 18:14:45 +0300

    Fix lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -72,6 +72,11 @@ module.exports = [
       'arrow-spacing': ['error', { 'before': true, 'after': true }],
       'no-console': 'error',
       'react/prop-types': 0
+    },
+    env: {
+    node: true,
+    browser: true
+  
     }
   }
 ]
