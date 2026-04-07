fc0d8814d5d85657a2c653982a685ab48cfb8482
Md Kamre Aallam <mdkamreaallam786@gmail.com>
2026-04-07 09:47:08 +0300

    Fix ESLint syntax error

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,80 @@
-{
-  files: ['src/**/*.{js,jsx}', 'test/**/*.{js,jsx}'],
-  plugins: {
-    react,
-    jest
+const js = require('@eslint/js')
+const react = require('eslint-plugin-react')
+const jest = require('eslint-plugin-jest')
+const globals = require('globals')
+
+module.exports = [
+  {
+    ignores: [
+      'webpack.config.js',
+      'eslint.config.js',
+      '.eslintrc.js',
+      'node_modules/**',
+      'dist/**'
+    ]
   },
-  languageOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
+
+  js.configs.recommended,
+
+  {
+    files: ['app.js'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node
       }
     },
-    globals: {
-      ...globals.browser,
-      ...globals.es6,
-      ...globals.jest
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 0
     }
   },
-  settings: {
-    react: {
-      version: 'detect'
+
+  {
+    files: ['src/**/*.{js,jsx}', 'test/**/*.{js,jsx}'],
+    plugins: {
+      react,
+      jest
+    },
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es6,
+        ...globals.jest
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'error',
+      'react/prop-types': 0
     }
-  },
-  rules: {
-    ...react.configs.recommended.rules,
-    'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'never'],
-    'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 'error',
-    'react/prop-types': 0
   }
-}
+]
